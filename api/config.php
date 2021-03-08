7c7a54cc52778bb571e04254896697fc5f7adf16
Azra <macbookair@MacBooks-MacBook-Air.local>
2021-03-08 18:05:50 +0100

    Initial commit of starter repo

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config {
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "autoresponder";
+  const DB_PASSWORD = "autoresponder";
+  const DB_SCHEME = "autoresponder";
+}
+
+?>
