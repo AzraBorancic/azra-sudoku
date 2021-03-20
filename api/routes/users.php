73768b42774cd2419e1d5ec154e6b288f7c5ef74
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-03-20 15:24:28 +0100

    Added routes for register and confirm.

--- api/routes/users.php
+++ api/routes/users.php
@@ -1,5 +1,20 @@
 <?php
 
 Flight::route('GET /users', function(){
-    Flight::json(Flight::userService()->get_users("SELECT * FROM users", NULL));
+    $offset = Flight::query('offset', 0);
+    $limit = Flight::query('limit', 25);
+    $search = Flight::query('search');
+    $order = Flight::query('order', "-id");
+
+    Flight::json(Flight::userService()->get_users($offset, $limit, $order));
+});
+
+Flight::route('POST /users/register', function(){
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::userService()->register($data));
+});
+
+Flight::route('GET /users/confirm/@token', function($token){
+    Flight::userService()->confirm($token);
+    Flight::json(["message" => "Your account has been activated"]);
 });
