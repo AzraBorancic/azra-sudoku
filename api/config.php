dff90cddfb660f75f521a5a95144160eb51907ee
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-03-20 13:31:55 +0100

    Added database properties to config.php

--- api/config.php
+++ api/config.php
@@ -2,9 +2,9 @@
 
 class Config {
   const DB_HOST = "localhost";
-  const DB_USERNAME = "autoresponder";
-  const DB_PASSWORD = "autoresponder";
-  const DB_SCHEME = "autoresponder";
+  const DB_USERNAME = "root";
+  const DB_PASSWORD = "root123";
+  const DB_SCHEME = "sudoku_game";
 }
 
 ?>
