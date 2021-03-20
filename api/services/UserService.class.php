05f3c329a98319d325f74a2cb1f51fc88d80f09d
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-03-20 15:24:01 +0100

    Completed base service and user service.

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -9,8 +9,48 @@ class UserService extends BaseService{
         $this->dao = new UserDao();
     }
 
-    public function get_users($query, $params){
-        return $this->list($query, $params);
+    public function get_users($offset, $limit, $order){
+        return $this->get_all($offset, $limit, $order);
+    }
+
+    public function register($user){
+        $this->dao->beginTransaction();
+
+        try {
+
+            $user = parent::add([
+                "name" => $user['name'],
+                "surname" => $user['surname'],
+                "email" => $user['email'],
+                "password" => $user['password'],
+                "status" => "PENDING",
+                "created_at" => date(Config::DATE_FORMAT),
+                "token" => md5(random_bytes(16))
+            ]);
+
+        } catch (\Exception $e) {
+            $this->dao->rollBack();
+            if (str_contains($e->getMessage(), 'users.eq_user_email')) {
+                throw new Exception("Account with same email exists in the database", 400, $e);
+            }else{
+                throw new Exception("Invalid request body!", 400, $e);
+            }
+        }
+        $this->dao->commit();
+
+        // TODO: send email with some token
+
+        return $user;
+    }
+
+    public function confirm($token){
+        $user = $this->dao->get_user_by_token($token);
+
+        if (!isset($user['id'])) throw Exception("Invalid token");
+
+        $this->dao->update($user['id'], ["status" => "ACTIVE"]);
+
+        //TODO send email to customer
     }
 
 }
