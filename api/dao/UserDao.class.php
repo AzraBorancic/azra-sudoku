6aaaa886ea25743eb0a682c1a40150c5887844de
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-03-20 15:23:31 +0100

    Completed base dao and user dao implementation

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -6,5 +6,17 @@ class UserDao extends BaseDao{
         parent::__construct("users");
     }
 
+    public function get_user_by_email($email){
+        return $this->query_unique("SELECT * FROM users WHERE email = :email", ["email" => $email]);
+    }
+
+    public function update_user_by_email($email, $user){
+        $this->execute_update("users", $email, $user, "email");
+    }
+
+    public function get_user_by_token($token){
+        return $this->query_unique("SELECT * FROM users WHERE token = :token", ["token" => $token]);
+    }
+
 }
 ?>
