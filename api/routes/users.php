adb5205037a4f29761c35d84901b0dd871391045
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-03-23 12:54:26 +0100

    Added Swagger documentation for users.

--- api/routes/users.php
+++ api/routes/users.php
@@ -1,47 +1,79 @@
 <?php
 
 /**
- * @OA\Get(
- *     path="/users",
- *     @OA\Response(response="200", description="List users form database"))
+ * @OA\Get(path="/users", tags={"users"},
+ *     @OA\Parameter(type="integer", in="query", name="offset", default=0, description="Offset for pagination"),
+ *     @OA\Parameter(type="integer", in="query", name="limit", default=25, description="Limit for pagination"),
+ *     @OA\Parameter(type="string", in="query", name="order", default="-id", description="Sorting for return elements: -column_name for ascedning order or +column_name for descending order"),
+ *     @OA\Response(response="200", description="List users from database")
+ * )
  */
-
 Flight::route('GET /users', function(){
     $offset = Flight::query('offset', 0);
     $limit = Flight::query('limit', 25);
-    $search = Flight::query('search');
+/*    $search = Flight::query('search');*/
     $order = Flight::query('order', "-id");
 
     Flight::json(Flight::userService()->get_users($offset, $limit, $order));
 });
 
 /**
- * @OA\Info(title="Sudoku game API", version="0.1")
+ * @OA\Get(path="/users/{id}", tags={"users"},
+ *     @OA\Parameter(@OA\Schema(type="integer"), in="path", name="id", default=1, description="Get user by id"),
+ *     @OA\Response(response="200", description="Fetch individual account")
+ * )
  */
+Flight::route('GET /users/@id', function($id){
+    Flight::json(Flight::userService()->get_by_id($id));
+});
 
 /**
- * @OA\Post(
- *     path="/users/register",
- *     @OA\Response(response="200", description="Add user to dabase"))
+ * @OA\Post(path="/users/register", tags={"users"},
+ *   @OA\RequestBody(description="Basic user info",required=true,
+ *     @OA\MediaType(mediaType="application/json",
+ *    	 @OA\Schema(
+ *    	   @OA\Property(property="name", type="string", example=""),
+ *         @OA\Property(property="surname", type="string", example=""),
+ *         @OA\Property(property="email", type="string", example=""),
+ *         @OA\Property(property="password", type="string", example="")
+ *          )
+ *      )
+ * ),
+ * @OA\Response(response="200", description="Add user to database")
+ * )
  */
-
 Flight::route('POST /users/register', function(){
     $data = Flight::request()->data->getData();
     Flight::json(Flight::userService()->register($data));
 });
 
 /**
- * @OA\Info(title="Sudoku game API", version="0.1")
- */
-
-/**
- * @OA\Get(
- *     path="/users/confirm/{token}",
- *     @OA\Parameter(@OA\Schema(type="string"), in="path", allowReserved=true, name="token", example=1))
+ * @OA\Get(path="/users/confirm/{token}", tags={"users"},
+ *     @OA\Parameter(type="string", in="path", allowReserved=true, name="token", example=1)),
  *     @OA\Response(response="200", description="Get user account status")
  */
-
 Flight::route('GET /users/confirm/@token', function($token){
     Flight::userService()->confirm($token);
     Flight::json(["message" => "Your account has been activated"]);
+});
+
+/**
+ * @OA\Put(path="/users/{id}", tags={"users"},
+ *   @OA\Parameter(@OA\Schema(type="integer"), in="path", name="id", default=1),
+ *   @OA\RequestBody(description="Basic account info that is going to be updated", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				@OA\Property(property="name", type="string", example=""),
+ *                  @OA\Property(property="surname", type="string", example=""),
+ *                  @OA\Property(property="email", type="string", example=""),
+ *                  @OA\Property(property="password", type="string", example="")
+ *          )
+ *       )
+ *     ),
+ *     @OA\Response(response="200", description="Update account based on id")
+ * )
+ */
+Flight::route('PUT /users/@id', function($id){
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::userService()->update($id, $data));
 });
--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -12,6 +12,14 @@ class UserService extends BaseService{
         return $this->get_all($offset, $limit, $order);
     }
 
+  /*  public function get_users($search, $offset, $limit, $order){
+        if ($search){
+            return $this->dao->get_users($search, $offset, $limit, $order);
+        }else{
+            return $this->dao->get_all($offset, $limit, $order);
+        }
+    }*/
+
     public function register($user){
         $this->dao->beginTransaction();
 
