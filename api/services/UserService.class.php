b897ea9c9caaccd5d1f95313c35db77057af3c39
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-04-11 21:25:37 +0200

    added reset and forgot password methods.

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -15,13 +15,29 @@ class UserService extends BaseService{
         return $this->get_all($offset, $limit, $order);
     }
 
-  /*  public function get_users($search, $offset, $limit, $order){
-        if ($search){
-            return $this->dao->get_users($search, $offset, $limit, $order);
-        }else{
-            return $this->dao->get_all($offset, $limit, $order);
-        }
-    }*/
+    public function reset($user){
+        $db_user = $this->dao->get_user_by_token($user['token']);
+
+        if (!isset($db_user['id'])) throw new Exception("Invalid token", 400);
+
+        if (strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user['token_created_at']) > 300) throw new Exception("Token expired", 400);
+
+        $this->dao->update($db_user['id'], ['password' => md5($user['password']), 'token' => NULL]);
+
+        return $db_user;
+    }
+
+    public function forgot($user){
+        $db_user = $this->dao->get_user_by_email($user['email']);
+
+        if (!isset($db_user['id'])) throw new Exception("User doesn't exists", 400);
+
+        if (strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user['token_created_at']) < 300) throw new Exception("Be patient tokens is on his way", 400);
+
+        $db_user = $this->update($db_user['id'], ['token' => md5(random_bytes(16)), 'token_created_at' => date(Config::DATE_FORMAT)]);
+
+        $this->smtpClient->send_user_recovery_token($db_user);
+    }
 
     public function login($user){
         $db_user = $this->dao->get_user_by_email($user['email']);
@@ -72,7 +88,7 @@ class UserService extends BaseService{
 
         $this->dao->update($user['id'], ["status" => "ACTIVE"]);
 
-        //TODO send email to customer
+        return $user;
     }
 
 }
