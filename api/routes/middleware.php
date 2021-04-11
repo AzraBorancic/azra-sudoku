6366ddfb747a0354afd1fd945e974710ff0c9e9e
AzraBorancic <azra.borancic@stu.ibu.edu.ba>
2021-04-11 21:15:48 +0200

    added middleware for admin users.

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,16 +1,15 @@
 <?php
-Flight::route('*', function (){
-    if(str_starts_with(Flight::request()->url, '/users/')) return true;
-
-    $headers = getallheaders();
-    $token = @$headers['Authorization'];
-
-    try{
-        $decoded = (array)\Firebase\JWT\JWT::decode($token, "JWT SECRET", ['HS256']);
-        Flight::set('user', $decoded);
+/* middleware for admin users */
+Flight::route('/admin/*', function(){
+    try {
+        $user = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"),"JWT_SECRET", ["HS256"]);
+        if ($user['r'] != "ADMIN"){
+            throw new Exception("Admin access required", 403);
+        }
+        Flight::set('user', $user);
         return true;
-    } catch(\Exception $e){
-        Flight::json(["Error" => $e->getMessage()], 401);
+    } catch (\Exception $e) {
+        Flight::json(["message" => $e->getMessage()], 401);
         die;
     }
 });
